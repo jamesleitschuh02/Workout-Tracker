4ab8866db7f49996b0c74d92cfecafbc27df14c1
James Leitschuh <james.leitschuh02@gmail.com>
2021-02-06 15:26:46 -0600

    added code to server.js to connect to mongo database

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const mongojs = require("mongojs");
+const logger = require("morgan");
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+const databaseUrl = "workout";
+const collections = ["workoutSeed"];
+
+const db = mongojs(databaseUrl, collections);
