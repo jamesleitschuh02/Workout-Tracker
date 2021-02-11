fd8bbe4ed26093a47b63e8e05f8ad7408fef62ce
James Leitschuh <james.leitschuh02@gmail.com>
2021-02-10 19:12:12 -0600

    added virtual function to workout model

--- models/workout.js
+++ models/workout.js
@@ -1,39 +1,51 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const WorkoutSchema = new Schema({
-        day: {
-          type: Date,
-          default: Date.now,
-        },
-    
-        exercises: [
-          {
-            type: {
-              type: String
-            },
-            name: {
-              type: String
-            },
-            duration: {
-              type: Number,
-            },
-            weight: {
-              type: Number,
-            },
-            reps: {
-              type: Number,
-            },
-            sets: {
-              type: Number,
-            },
-            distance: {
-              type: Number,
-            },
-          },
-        ],
-      });
+const WorkoutSchema = new Schema(
+  {
+    day: {
+      type: Date,
+      default: Date.now,
+    },
 
-const Workout = mongoose.model("Workout",WorkoutSchema);
+    exercises: [
+      {
+        type: {
+          type: String,
+        },
+        name: {
+          type: String,
+        },
+        duration: {
+          type: Number,
+        },
+        weight: {
+          type: Number,
+        },
+        reps: {
+          type: Number,
+        },
+        sets: {
+          type: Number,
+        },
+        distance: {
+          type: Number,
+        },
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+workoutSchema.virtual("totalDuration").get(function () {
+  return this.exercises.reduce((total, exercise) => {
+    return total + exercise.duration;
+  }, 0);
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
 
 module.exports = Workout;
--- routes/db-routes.js
+++ routes/db-routes.js
@@ -5,4 +5,39 @@ module.exports = function (app) {
     });
     res.json(workout);
   });
+
+  app.put("/api/workouts/:id", (req, res) => {
+    let id = req.params.id;
+    let body = req.body;
+    db.Workout.findByIdAndUpdate(
+      id,
+      { $push: { exercises: body } },
+      { new: true }
+    )
+      .then((data) => {
+        res.json(data);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(404).json(err);
+      });
+  });
+
+  // createWorkout(data)
+  app.post("/api/workouts", ({ body }, res) => {
+    db.Workout.create(body).catch((err) => {
+      console.error(err);
+      res.json(err);
+    });
+    console.log(workout);
+    res.json(workout);
+  });
+
+  // getWorkoutInRange()
+  app.get("/api/workouts/range", (_req, res) => {
+    db.Workout.find({}).catch((err) => {
+      res.status(404).json(err);
+    });
+    res.json(workout);
+  });
 };
